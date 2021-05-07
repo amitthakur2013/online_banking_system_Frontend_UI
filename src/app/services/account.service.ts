a788b9dea101bdbcbc2a7e121c161da79017780c
amitthakur2013 <amitthakur2013@gmail.com>
2021-05-07 15:56:11 +0530

    refactored a bit

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -11,17 +11,14 @@ private baseUrl="http://localhost:8080/banking/account";
   constructor(private httpClient: HttpClient) { }
 
   getUserDetails(): Observable<any>{
-  	//return this.httpClient.get<any>(`${this.baseUrl}/dashboard`);
   	return this.httpClient.get<any>("api/banking/account/dashboard");
   }
 
   getAccountDetails(id:number): Observable<any> {
-  	//return this.httpClient.get<any>(`${this.baseUrl}/details/${id}`);
   	return this.httpClient.get<any>(`api/banking/account/details/${id}`);
   }
 
   getMiniStatement(id:number):  Observable<any> {
-  	//return this.httpClient.get<any>(`${this.baseUrl}/mini_statement/${id}`);
   	return this.httpClient.get<any>(`api/banking/account/mini_statement/${id}`);
   }
 
